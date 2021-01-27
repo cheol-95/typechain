d524cd6051b41fd04c1e49508e67694d574d0af7
cheol-95 <rkdcjf0122@gmail.com>
2021-01-27 15:09:52 +0900

    src/ dist Dir 추가

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = 'cheol', age = 24, gender = 'male';
-const sayHi = (name, age, gender) => {
-    console.log('name,age,gender: ', name, age, gender);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return 'name,age,gender:';
+};
+
+console.log(sayHi('cheol', 44, 'male'));
+
+export {};

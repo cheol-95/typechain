54e839d40c2cfdd6c8f6b64d8291d6ed8d48a914
cheol-95 <rkdcjf0122@gmail.com>
2021-01-27 14:12:47 +0900

    ts setting

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = 'cheol',
+  age = 24,
+  gender = 'male';
+
+const sayHi = (name, age, gender) => {
+  console.log('name,age,gender: ', name, age, gender);
+};
+
+sayHi(name, age);
+
+export {};

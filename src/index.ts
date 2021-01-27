7cc6e5f685098475c1bba8bb064dadca4dca239e
cheol-95 <rkdcjf0122@gmail.com>
2021-01-27 16:52:09 +0900

    ts class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return 'name,age,gender:';
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    // constructor -> 클래스가 생성 될때마다 실행되는 함수
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const person = {
+  name: 'cheol',
+  gender: 'male',
+  age: 22,
 };
 
-console.log(sayHi('cheol', 44, 'male'));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
